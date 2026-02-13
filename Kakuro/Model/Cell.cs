31f32e8630dfc7ca285d564785463d70f56e6b86
ALPHVMANE <alphxrizen@gmail.com>
2026-02-13 13:31:58 -0500

    Updated: Refator Models + .gitignore file

---
+++ Kakuro/Model/Cell.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kakuro.Model
+{
+    public abstract class Cell
+    {
+        private int x;
+        private int y;
+
+        public Cell(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+
+    }
+
+    public class Clue : Cell
+    {
+        private int? horizontalSumClue;
+        private int? verticalSumClue;
+
+        public Clue(int x, int y, int? horizontalSumClue, int? verticalSumClue) : base(x, y)
+        {
+            this.horizontalSumClue = horizontalSumClue;
+            this.verticalSumClue = verticalSumClue;
+        }
+
+        public int? HorizontalSumClue { get => horizontalSumClue; set => horizontalSumClue = value; }
+        public int? VerticalSumClue { get => verticalSumClue; set => verticalSumClue = value; }
+    }
+
+    public class Entry : Cell
+    {
+        private int? currentValue;
+        private int correctValue; // validation check
+
+        public Entry(int x, int y, int? currentValue, int correctValue) : base(x, y)
+        {
+            this.currentValue = currentValue;
+            this.correctValue = correctValue;
+        }
+
+        public int? CurrentValue { get => currentValue; set => currentValue = value; }
+        public int CorrectValue { get => correctValue; set => correctValue = value; }
+    }
+
+    public class Blacked : Cell
+    {
+        public Blacked(int x, int y) : base(x, y)
+        {
+        }
+    }
+
+}
--- Kakuro/Model/User.cs
+++ Kakuro/Model/User.cs
@@ -11,23 +11,40 @@ namespace Kakuro.Model
         private string username;
         private string password;
         private int score;
-        private string rank;
+        private int rank;
+        private int levelsCompleted;
 
-        public User(string username, string email, string password, int score, string rank)
+        public User(string email, string username, string password)
+        {
+            this.email = email;
+            this.username = username;
+            this.password = password;
+            this.score = 0;
+            this.rank = CalculateRank();
+            this.levelsCompleted = 0;
+
+        }
+
+        public User(string username, string email, string password, int score, int rank, int levelsCompleted)
         {
             this.username = username;
             this.email = email;
-            this.Password = password;
+            this.password = password;
             this.score = score;
             this.rank = rank;
+            this.levelsCompleted = levelsCompleted;
         }
 
         public string Email { get => email; set => email = value; }
         public string Username { get => username; set => username = value; }
         public string Password { get => password; set => password = value; }
         public int Score { get => score; set => score = value; }
-        public string Rank { get => rank; set => rank = value; }
+        public int Rank { get => rank; set => rank = value; }
+        public int LevelsCompleted { get => levelsCompleted; set => levelsCompleted = value; }
 
-
+        private int CalculateRank()
+        {
+            //Coming Soon....
+        }
     }
 }
